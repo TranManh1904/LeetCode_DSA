8c1c8e6f20390190112d52b79a6b53ce548c39e4
TranDucManh <tranducmanh6303@gmail.com>
2025-09-03 20:52:17 +0700

    solution 3 third maximum number

--- 414_Third_maximum_number.cpp
+++ 414_Third_maximum_number.cpp
@@ -61,10 +61,41 @@ void solution2(int numbers[], int n){
 	cout << "third number: " << third;
 }
 
+void solution3(int numbers[], int n){
+	int first = INT_MIN; // vd fist = -10
+	int second = INT_MIN; // vd second = -10
+	int third = INT_MIN; // vd third = -10
+	// 2,7,6,5,7,3
+
+	for (int i = 0; i < n; i++){
+		if (numbers[i]>first){ // vd 2 > -10
+			third = second; // -10 = -10  
+			second = first; // luc nay first = -10 => second = -10
+			first = numbers[i]; //number[i] = 2 => first = 2
+		}
+		else if (numbers[i] > second && numbers[i] != first){ // i = 1, numbers[1] = 7 > second(-10) && 7 != first(2)
+			third = second;
+			second = numbers[i];
+		}
+		else if (numbers[i] > third && numbers[i] != second && numbers[i] != first){
+			third = numbers[i];
+		}
+	}
+	cout << endl;
+	cout << "first: " << first;
+	cout << endl;
+	cout << "second: " << second;
+	cout << endl;
+	cout << "third number: " << third;
+}
 
 int main(){
 	int numbers[] = {2,7,6,5,7,3};
 	int n = 6;
 	solution1(numbers, n);
 	solution2(numbers, n);
+	cout << endl;
+	cout << "solution 3: ";
+	solution3(numbers, n);
+	return 0;
 }
